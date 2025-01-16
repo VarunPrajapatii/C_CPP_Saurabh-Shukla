efa67be13516e2989c97342e8d872245f56e4790
Varun Prajapati <varunprajapati123@gmail.com>
2025-01-16 12:06:35 +0530

    merge sort code added

--- MySirG - C++ - DSA/Assignments/22Sorting.cpp
+++ MySirG - C++ - DSA/Assignments/22Sorting.cpp
@@ -157,7 +157,55 @@ int partition(int *a, int p, int q)
 
 
 
+// MERGE SORT
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void merge(vector<int> &arr, int start, int mid, int end) {
+//     vector<int> temp(end-start+1);
+//     int i=start, j=mid+1, index=0;
+//     while(i<=mid && j<=end) {
+//         if(arr[i] <= arr[j]) {
+//             temp[index] = arr[i];
+//             i++;
+//         } else {
+//             temp[index] = arr[j];
+//             j++;
+//         }
+//         index++;
+//     }
+//     while(i <= mid) {
+//         temp[index] = arr[i];
+//         i++;
+//         index++;
+//     }
+
+//     while(j <= end) {
+//         temp[index] = arr[j];
+//         j++;
+//         index++;
+//     }
+//     index=0;
+//     while(start<=end) {
+//         arr[start] = temp[index];
+//         start++, index++;
+//     }
+// }
+
+// void mergeSort(vector<int> &arr, int start, int end) {
+//     if(start >= end) return;
+//     int mid = start + (end-start)/2;
+//     mergeSort(arr, start, mid);
+//     mergeSort(arr, mid+1, end);
+//     merge(arr, start, mid, end);
+// }
 
 
-
-
+// int main() {
+//     vector<int> nums={6,3,5,2,2,8,1,3,2,9};
+//     mergeSort(nums, 0, nums.size()-1);
+//     for(int i=0; i<nums.size(); i++) {
+//         cout<<nums[i]<<" ";
+//     }
+//     return 1;
+// }
